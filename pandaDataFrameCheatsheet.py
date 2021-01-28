5e4eb20a60fab2cd8e118a0231410c99fe52294d
anne marie yao xian <72063837+chinamyx@users.noreply.github.com>
2021-01-28 18:38:49 +0800

    Update pandaDataFrameCheatsheet.py

--- pandaDataFrameCheatsheet.py
+++ pandaDataFrameCheatsheet.py
@@ -5,11 +5,7 @@ y = pd.DataFrame(0.0001*np.random.randint(0,10000,size=(100,100)))
 y.to_csv(r"Coursera\ppd1\write1.csv")
 y = pd.read_csv(r"Coursera\ppd1\write1.csv", index_col = 0) #index_col to remove unnamed columns #read converts columns to string
 y = y.sort_values(['0','1','2'])
-
 y.to_csv(r"Coursera\ppd1\write1.csv")
-
-
-
 columns_titles = y.columns.values
 
 df_reorder=df.reindex(columns=columns_titles)
