b6c6862b12132c95476c08f5fcba6df75a50b98e
anne marie yao xian <72063837+chinamyx@users.noreply.github.com>
2021-01-28 18:35:59 +0800

    Create PandaDataFrameCheatSheet2.py

---
+++ PandaDataFrameCheatSheet2.py
@@ -0,0 +1,35 @@
+# import pandas as pd
+
+# url ='https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/Chapter%204/Datasets/TopSellingAlbums.csv'
+
+# df = pd.read_csv(url)
+
+# # print(df.head())
+
+# print(df)
+# print("\n \n test new slice \n \n")
+# print("df.loc[x:x,'label1:label2]")
+# print(df.loc[0:2,'Genre':'Rating'])
+
+# print("\n \n test new slice \419094n \n")
+
+# print(f"\n",50*"*","\n")
+# print("df.loc[0:x,0:y]")
+# print(df.loc[0:2,'Genre':'Rating'])
+
+# print(f"\n",50*"*","\n")
+# print("df.iloc[0:x,0:y] -- iloc x-1, y -1 , row followed by column")
+# print(df.iloc[0:2,0:3])
+
+
+# # print(df[['Length']])
+# # print(df)
+
+
+# print(f"\n",50*"*","\n")
+# new_index=['a','b','c','d','e','f','g','h']
+# df_new=df 
+# df_new.index=new_index 
+# # df_new.loc['a', 'Artist'] 
+# # df_new.loc['a':'d', 'Artist'] 
+# print(df_new)
